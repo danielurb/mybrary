775c297f8f6da667a984f259b69026c6210028f1
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2020-12-21 07:27:44 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
